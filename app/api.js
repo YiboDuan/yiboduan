8f1246d6c0ebd07faf5477fb1e50100b60f43677
yiboduan <yibo.duan@gmail.com>
2016-09-15 02:49:37 -0500

    check sha

--- app/api.js
+++ app/api.js
@@ -1,12 +1,18 @@
 module.exports = function(router) {
     var exec = require('child_process').exec;
-
+    var crypto = require('crypto');
     // receive json from git webhook
     router.route('/api/push').post(function(req, res) {
         console.log(req.headers)
         console.log(req.body);
-        console.log(req.headers['x-hub-signature'])
-        // wow
+
+        if(req.headers['x-hub-signature'] !== undefined) {
+            const hash = crypto.createHmac('sha1', process.env.SECRET_TOKEN)
+                   .update(req.body)
+                   .digest('hex');
+            console.log(req.headers['x-hub-signature'])
+            console.log('sha1=' + hash);
+        }
 
         var cmd = `cd ~/${req.body.repository.name} && git pull && sleep 5 && npm install && pm2 restart app`
         exec(cmd, function (error, stdout, stderr) {
